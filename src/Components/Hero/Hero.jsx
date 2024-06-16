47af0f8f813254e4845d1f54cb851a942144e14d
Nandan Pai <100035968+theDevPai@users.noreply.github.com>
2024-06-16 23:32:21 +0530

    Update Hero.jsx

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
   return (
     <div className='hero container'>
         <div className="hero-text">
-            <h1>We Ensure to help you produce fresh O<sub>2</sub></h1>
+            <h1>We Ensure To Help You Produce Fresh O<sub>2</sub></h1>
             <p>Our platform is designed to empower the public with the data and techniques to calculate the amount of O<sub>2</sub> that can be produced
                 by our humble initiave. You're one simple calculation away from finding how many trees you need to plant to make this planet more sustainable.
             </p>
