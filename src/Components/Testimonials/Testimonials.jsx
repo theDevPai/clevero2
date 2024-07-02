e4f9e8fd5afea21db08d46d87e46fef487a10d6d
Nandan Pai <100035968+theDevPai@users.noreply.github.com>
2024-07-03 01:07:03 +0530

    Update Testimonials.jsx

--- src/Components/Testimonials/Testimonials.jsx
+++ src/Components/Testimonials/Testimonials.jsx
@@ -56,30 +56,6 @@ const slideBackward = () => {
                         <p>"CleverO2 is an invaluable resource for anyone interested in environmental sustainability. The detailed database of global tree species and the carbon reduction calculator provide essential insights for planting projects. It's an excellent tool for understanding the impact of afforestation on carbon emissions and promoting greener initiatives."</p>
                     </div>
                 </li>
-                <li>
-                    <div className="slide">
-                        <div className="user-info">
-                            <img src={usr3} alt="" srcset="" />
-                            <div>
-                                <h3>Pranav Tippimath</h3>
-                                <span>BMSCE, India</span>
-                            </div>
-                        </div>
-                        <p>Officia labore enim enim eiusmod et in irure culpa. Enim cillum amet ut deserunt officia enim enim deserunt officia officia officia. Eiusmod consequat ea quis labore adipisicing sunt cupidatat cillum sit. Commodo tempor laboris aliquip est proident adipisicing excepteur. Est et tempor officia anim. Aute pariatur elit reprehenderit in veniam nulla id aute enim enim nostrud consequat amet. Irure ex exercitation tempor eu ex cupidatat pariatur elit proident nostrud cillum.</p>
-                    </div>
-                </li>
-                <li>
-                    <div className="slide">
-                        <div className="user-info">
-                            <img src={usr4} alt="" srcset="" />
-                            <div>
-                                <h3>Donald Trump</h3>
-                                <span>White House,Washington DC, USA</span>
-                            </div>
-                        </div>
-                        <p>Officia labore enim enim eiusmod et in irure culpa. Enim cillum amet ut deserunt officia enim enim deserunt officia officia officia. Eiusmod consequat ea quis labore adipisicing sunt cupidatat cillum sit. Commodo tempor laboris aliquip est proident adipisicing excepteur. Est et tempor officia anim. Aute pariatur elit reprehenderit in veniam nulla id aute enim enim nostrud consequat amet. Irure ex exercitation tempor eu ex cupidatat pariatur elit proident nostrud cillum.</p>
-                    </div>
-                </li>
             </ul>
         </div>
     </div>
