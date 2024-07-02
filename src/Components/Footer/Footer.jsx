88c00b66ac66fa0c22af52ac57c82ca09f1fe035
Nandan Pai <100035968+theDevPai@users.noreply.github.com>
2024-07-03 01:30:30 +0530

    Update Footer.jsx

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import './footer.css'
 const Footer = () => {
   return (
     <div className='footer'>
-       <p>Registered. 2024 CleverO2 Pvt. Ltd. All Rights Reserved</p> 
+       <p>2024 CleverO2 Pvt. Ltd. All Rights Reserved</p> 
        <ul>
         <li>Terms of Service</li>
         <li>Privacy Policy</li>
