0418adc8d7dc4c48315600339d26457af8dc1b02
Nandan Pai <100035968+theDevPai@users.noreply.github.com>
2024-07-03 01:05:05 +0530

    Update Testimonials.jsx

--- src/Components/Testimonials/Testimonials.jsx
+++ src/Components/Testimonials/Testimonials.jsx
@@ -40,7 +40,8 @@ const slideBackward = () => {
                                 <span>King's College, London, UK</span>
                             </div>
                         </div>
-                        <p>Officia labore enim enim eiusmod et in irure culpa. Enim cillum amet ut deserunt officia enim enim deserunt officia officia officia. Eiusmod consequat ea quis labore adipisicing sunt cupidatat cillum sit. Commodo tempor laboris aliquip est proident adipisicing excepteur. Est et tempor officia anim. Aute pariatur elit reprehenderit in veniam nulla id aute enim enim nostrud consequat amet. Irure ex exercitation tempor eu ex cupidatat pariatur elit proident nostrud cillum.</p>
+                        <p>"The CleverO2 website is a game-changer for ESG analysts like myself. It offers a comprehensive database of trees worldwide, making it effortless to find specific data for carbon emission reduction and removal calculations. This tool significantly simplifies the assessment of afforestation projects, ensuring precise and efficient evaluations."
+</p>
                     </div>
                 </li>
                 <li>
@@ -52,7 +53,7 @@ const slideBackward = () => {
                                 <span>BMSCE, India</span>
                             </div>
                         </div>
-                        <p>Officia labore enim enim eiusmod et in irure culpa. Enim cillum amet ut deserunt officia enim enim deserunt officia officia officia. Eiusmod consequat ea quis labore adipisicing sunt cupidatat cillum sit. Commodo tempor laboris aliquip est proident adipisicing excepteur. Est et tempor officia anim. Aute pariatur elit reprehenderit in veniam nulla id aute enim enim nostrud consequat amet. Irure ex exercitation tempor eu ex cupidatat pariatur elit proident nostrud cillum.</p>
+                        <p>"CleverO2 is an invaluable resource for anyone interested in environmental sustainability. The detailed database of global tree species and the carbon reduction calculator provide essential insights for planting projects. It's an excellent tool for understanding the impact of afforestation on carbon emissions and promoting greener initiatives."</p>
                     </div>
                 </li>
                 <li>
